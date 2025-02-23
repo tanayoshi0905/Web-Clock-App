89dcca73aafed8ce8e75fe7b9323ed8f255e42ae
田邉佳之 <yossy@tana-yossy-mac.local>
2025-02-23 16:05:03 +0900

    天気更新実装

--- weather.js
+++ weather.js
@@ -1,11 +1,15 @@
 const url = "https://weather.tsukumijima.net/api/forecast/city/350030";
 
-fetch(url)
-    .then(response =>  response.json())
-    .then(data => {
-        console.log(data);
-        document.getElementById("locate").textContent = data.title
-        document.getElementById("today_weather").textContent = data.forecasts[0].telop;
-        console.log(data.forecasts[0].image.url);
-        document.getElementById("weather_svg").src = data.forecasts[0].image.url;
-    })
+function getWeather(){
+    fetch(url)
+        .then(response =>  response.json())
+        .then(data => {
+            console.log(data);
+            document.getElementById("locate").textContent = data.title
+            document.getElementById("today_weather").textContent = data.forecasts[0].telop;
+            console.log(data.forecasts[0].image.url);
+            document.getElementById("weather_svg").src = data.forecasts[0].image.url;
+        })
+}
+
+setInterval(getWeather, 1800)
